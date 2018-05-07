31580774eccfb79fdf44620635d8849e297797fe
ZenosLin <zenoslin@outlook.com>
2018-05-07 20:46:04 +0800

    updata

--- web/db_config.php
+++ web/db_config.php
@@ -3,7 +3,7 @@
 
     $mysql_username='name'; //mysql数据库用户名
 
-    $mysql_password='password'; //mysql数据库密码,初始默认密码为空
+    $mysql_password='password'; //mysql数据库密码
 
-    $mysql_database='mydata'; //改成自己的mysql数据库名
+    $mysql_database='mydata'; //mysql数据库名
  ?>
